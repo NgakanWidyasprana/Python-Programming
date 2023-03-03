cbd0cd91aa6b6bdfc94f61d1442cd081660bd5cf
Ngakan Putu Widyasprana <ngakanputu39@gmail.com>
2023-03-03 12:08:02 +0800

    Update the optional feature

--- optional_feature1.py
+++ optional_feature1.py
@@ -1,1 +1,5 @@
-print("This is optional feature 1")
+def automate_generates_odd_numbers(number):
+    value = [ x for x in range(1,number+1) if x%2!=0]
+    return value
+
+print(automate_generates_odd_numbers(101))
