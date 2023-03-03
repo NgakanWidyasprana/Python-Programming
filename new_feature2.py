de4b1f892f5cdda13226683070e18f5107638ddd
Ngakan Putu Widyasprana <ngakanputu39@gmail.com>
2023-03-03 12:00:12 +0800

    Update the feature 2

--- new_feature2.py
+++ new_feature2.py
@@ -1,1 +1,13 @@
-print("This is new feature 2")
+def list(nama):
+    list_nama = []
+    for domain, body in nama.items():
+        for body in body:
+            list_nama.append("{}{}".format(body,domain))
+    return list_nama
+
+nama = {
+    "@gmail.com":["ngakan","ngakan37","william"],
+    "@outlook.com":["diah","pramesti","diahpram68"]
+}
+
+print(list(nama))
