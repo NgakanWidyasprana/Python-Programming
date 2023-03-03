686de8bff8821c6735f17d56af394ac95c1975ec
Ngakan Putu Widyasprana <ngakanputu39@gmail.com>
2023-03-03 11:45:53 +0800

    Update the feature 1

--- new_feature1.py
+++ new_feature1.py
@@ -1,1 +1,8 @@
-print("This is feature 1")
+def list(name): 
+    return name.split()
+
+def print_name(value):
+    print("Nama panggilan saya adalah {}. Nama bali saya adalah {}".format(value[0],value[1]))
+
+list_name= print_name(list("Ngakan Putu"))
+list_name= print_name(list("William Wayan"))
